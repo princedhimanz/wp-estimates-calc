2343d769fa2bb41714e3061332eea7ff61748a2a
princedhimanz <jandu25@gmail.com>
2021-09-26 15:53:12 +0530

    1 file modified

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="calculator">
     <div class="categories">
-      <h3>1.Chxoose A Category</h3>
+      <h3>1.Choose A Category</h3>
     </div>
     <div class="items">
       <h3>2.Click on your items for estimation</h3>
