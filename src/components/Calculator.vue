6f12089a8f1710eb45a7019e227c1f0a0bc39371
princedhimanz <jandu25@gmail.com>
2021-09-29 10:28:58 +0000

    Added Design

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -3,38 +3,76 @@
     <div class="categories">
       <h3>1.Choose A Category</h3>
       <ul>
-        <li v-for="category of categories" :key="category.name">
-          {{category.name}}
+        <li
+          v-for="category of categories"
+          :key="category.name"
+          @click="activeCategory = category.name"
+          :class="{active:activeCategory === category.name}"
+        >
+          {{ category.name }}
         </li>
       </ul>
     </div>
     <div class="items">
       <h3>2.Click on your items for estimation</h3>
-       <div class="itemsContainer">
-        <div v-for="item of categories[0].items" :key="item.name" class="item">
-          <div>{{item.name}}</div>
+      <div class="itemsContainer">
+        <div v-for="item of activeCategoryItems" :key="item.name" class="item">
+          <div>{{ item.name }}</div>
           <NumberInputSpinner
             :min="0"
             :max="10"
-            :step="2"
+            :step="1"
             :integerOnly="true"
             v-model="yourVModel"
           />
+        </div>
       </div>
     </div>
-  </div>
+    <footer class="footer">
+      <div class="d-flex">
+        <h2>Logo Here</h2>
+        <div class="progressBar d-flex">
+          <h4>Capacity</h4>
+          <div style="width:300px"><progress-bar
+          size="huge"
+          val="60"
+          text="69m"
+          bar-color="#69b2c5"
+          bar-border-radius="20"
+          text-position="middle"
+          font-size="16"
+          />
+          </div>
+          <h4>Max</h4>
+          </div>
+      </div>
+      <div class="d-flex">
+        <h3>Cost: $123</h3>
+        <button class="bookBtn">Book Now</button>
+      </div>
+    </footer>
   </div>
 </template>
 
 <script>
 import NumberInputSpinner from 'vue-number-input-spinner';
+import ProgressBar from 'vue-simple-progress';
 
 export default {
   name: 'Calculator',
   components: {
     NumberInputSpinner,
+    ProgressBar,
+  },
+  computed: {
+    activeCategoryItems() {
+      return this.categories.filter(
+        (category) => category.name === this.activeCategory,
+      )[0].items;
+    },
   },
   data: () => ({
+    activeCategory: 'Furniture',
     categories: [
       {
         name: 'Furniture',
@@ -105,31 +143,58 @@ export default {
           {
             name: '2 Seater Sofa',
           },
-
         ],
       },
       {
         name: 'Household',
-        items: [],
+        items: [
+          {
+            name: '2 Seater Sofa',
+          },
+          {
+            name: '2 Seater Sofa',
+          },
+        ],
       },
       {
         name: 'Carpent & Flooring',
-        items: [],
+        items: [
+          {
+            name: '2 Seater Sofa',
+          },
+          {
+            name: '2 Seater Sofa',
+          },
+        ],
       },
       {
         name: 'Bedding & Window Dressing',
-        items: [],
+        items: [
+          {
+            name: '2 Seater Sofa',
+          },
+          {
+            name: '2 Seater Sofa',
+          },
+        ],
       },
       {
         name: 'Bathroom',
-        items: [],
+        items: [
+          {
+            name: '2 Seater Sofa',
+          },
+          {
+            name: '2 Seater Sofa',
+          },
+        ],
       },
     ],
   }),
 };
 </script>
 
-<style scoped>
+<style>
 .calculator {
   display: grid;
   grid-template-columns: 300px auto;
@@ -137,33 +202,77 @@ export default {
   gap: 2em;
 }
 
-ul{
+ul {
   list-style: none;
-  margin: 2em 0
+  margin: 2em 0;
 }
-.categories li{
-  background: #d3d3d3;
+.categories li {
+  background: #69b2c5;
   padding: 1em 2em;
-  margin-top:5px;
+  margin-top: 5px;
   font-weight: 600;
   cursor: pointer;
   transition: all 0.2s linear;
   box-shadow: 1px 1px 4px #ddd;
+  color: #fff;
 }
-.categories li:hover{
-  background: #b3b3b3
+.categories li:hover, .categories li.active {
+  background: #5592a2;
 }
-.itemsContainer{
+.itemsContainer {
   margin: 2em 0;
   display: grid;
   grid-template-columns: 1fr 1fr;
   gap: 1em;
   padding: 0 2em;
+  margin-bottom: 5em;
 }
-.item{
+.item {
   padding: 0.5em;
   display: flex;
   justify-content: space-between;
+  align-items: center;
 }
-
+.footer {
+  background: #ddd;
+  height: 5em;
+  width: 100%;
+  display: block;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 3em;
+}
+.d-flex {
+  display: flex;
+  align-items: center;
+}
+.bookBtn {
+  padding: 0.75em 2em;
+  border: 2px solid #387e90;
+  font-size: 18px;
+  font-weight: bold;
+  border-radius: 5px;
+  color: #244f5a;
+  background: #fff;
+  margin: 0 2em;
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+}
+.bookBtn:hover {
+  background: #387e90;
+  color:#fff
+}
+.progressBar{
+  margin-left:2em;
+}
+.vue-simple-progress{
+  margin: 0 1em;
+}
+.vue-simple-progress-text{
+  font-size: 18px !important;
+}
 </style>
